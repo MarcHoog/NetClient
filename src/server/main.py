f48c3c599b35f4010fb714a19b764f94537dc3ca
Marc Hoogendoorn <Marc.Hoogendoorn@simac.com>
2024-08-11 14:03:33 +0200

    Craeted server class

--- src/server/main.py
+++ src/server/main.py
@@ -19,73 +19,73 @@ import queue
 #[Client0] -> [listening Queue][Server]  - 
 #                                        \ - [sending Queu][Client2]
 
-NEW_CLIENT = 0
-NEW_MESSAGE = 1
-DISCONNECT = 2
+class Server():
+    def __init__(self, host, port):
+        self.clients = {}
+        self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.host = host
+        self.port = port
+        self.format = 'UTF-8'
+        self.header = 64 
 
-if __name__ == "__main__":
-    
-    setup_logger()
-    
-    HOST = '127.0.0.1'
-    PROT = 50009
-    HEADER = 64
-    FORMAT = 'UTF-8'
-    DISCONNECT_MESSAGE = "!DISCONNECT"
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((HOST, PROT))
+        self.server.bind((self.host, self.port))
+        self.master_queue = queue.Queue()
 
-    def client_thread(conn, addr, client_queue, master_queue):
+        self.setup_listening_thread()
+        self._master_thread()
+
+    def setup_listening_thread(self):
+        """
+        Sets up a listening thread that listens for incomming connections
+        """
+        thread = threading.Thread(target=self._listening_thread, args=())
+        thread.start()
+        logging.info(f'Active threads: {threading.active_count() - 1}')
+        return thread
+
+    def _listening_thread(self):
+        logging.info('listening thread has been started....')
+        logging.info('Listening for Connections')
+
+        self.server.listen()
+        while True:
+            conn, addr = self.server.accept()
+            client_queue = queue.Queue()
+            self.clients[addr] = client_queue
+            thread = threading.Thread(target=self._client_thread, args=(conn, addr, client_queue))
+            thread.start()
+
+
+    def _client_thread(self, conn, addr, client_queue):
         connected = True
         logging.info(f'Connected by {addr[0]}:{addr[1]}')
         while connected:
             readable, writeable, exception = select.select([conn], [], [])
             if readable:
-                msg_length = conn.recv(HEADER).decode(FORMAT)
+                msg_length = conn.recv(self.header).decode(self.format)
                 if msg_length:
                     msg_length = int(msg_length)
-                    msg = conn.recv(msg_length).decode(FORMAT)
+                    msg = conn.recv(msg_length).decode(self.format)
                     logging.info(f"Message recieved : '{msg}' From {addr[0]}:{addr[1]}")
-                    if msg == DISCONNECT_MESSAGE:
-                        connected = False
-                        
-            
+            if not client_queue.empty():
+                message = client_queue.get()
+                conn.send(message.encode(self.format))
+                logging.info(f"Message sent: '{message}' to {addr[0]}:{addr[1]}")
+
         conn.close()
-    
-    def listing_thread(master_queue):
-        server.listen()
-        logging.info('listening thread has been started....')
-        logging.info('Listening for Connections')
-        
-        while True:
-            conn, addr = server.accept()
-            logging.info('Connection Accepted')
-            client_queue = queue.Queue()
-            master_queue.put((NEW_CLIENT, (conn, addr, client_queue)))
-            thread = threading.Thread(target=client_thread, args=(conn, addr, client_queue, master_queue))
-            thread.start()
-            logging.info(f'Active threads: {threading.active_count() - 1}')
 
-    def setup_server():
-        master_queue = queue.Queue()
-        thread = threading.Thread(target=listing_thread, args=(master_queue, ))
-        thread.start()
-        logging.info(f'Active threads: {threading.active_count() - 1}')
-        return master_queue
 
-    def start():
-        master_queue = setup_server()
-        clients = {}        
-        
+    def _master_thread(self):
         while True:
-            if not master_queue.empty():
-                item = master_queue.get()
-                if item[0] == NEW_CLIENT:
-                    logging.info(f'New Client: {item[1][1][0]}:{item[1][1][1]} added to main thread')
-                    clients[f'{item[1][1][0]}:{item[1][1][1]}'] = item[1][2]
+            if not self.master_queue.empty():
+                item = self.master_queue.get()
+                if item['type'] == 'NewClient':
+                    logging.info(f'New Client: {item["addr"][0]}:{item["addr"][1]} added to main thread')
+                    self.clients[{item["addr"][0]}:{item["addr"][1]}] = item['client_queue']
             else:
                 pass
 
-
-
-    start()
+if __name__ == "__main__":
+    
+    setup_logger()
+    Server("127.0.0.1", 50009)
